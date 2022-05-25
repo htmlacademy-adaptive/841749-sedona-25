0dd441872fc44dcf60b5384dde50570b201039a5
Mihails Bodrins <m.bodrins@gmail.com>
2022-05-25 13:33:25 +0300

    минифицировал стили на всех страницах

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 
@@ -16,6 +17,7 @@ export const styles = () => {
       autoprefixer(),
       csso() // style.css[prefix] -> style.css[prefix, min]
     ]))
+    .pipe(rename ('style.min.css'))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
